1e7217f7ee26e5a9c95ea9293c95b47dba7b49a7
Vo Minh Duc <duc-vm@genkisystem.com>
2021-08-04 01:15:34 +0700

    add regex

--- packages/api/go.mod
+++ packages/api/go.mod
@@ -4,5 +4,7 @@ go 1.13
 
 require (
 	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-chi/chi/v4 v4.0.0-rc1 // indirect
+	github.com/go-chi/chi/v5 v5.0.3 // indirect
 	go.mongodb.org/mongo-driver v1.7.0
 )
--- packages/api/main.go
+++ packages/api/main.go
@@ -4,18 +4,27 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"poc/indigo/model"
 	"poc/indigo/repo"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 var mh *repo.MongoHandler
 
+type ResponseData struct {
+	patients []*model.Patient
+	total    int
+}
+
 func registerRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Route("/patients", func(r chi.Router) {
 		r.Get("/", getAllPatient)
+	})
+	r.Route("/get", func(r chi.Router) {
+		r.Get("/", getPatientsByName)
 	})
 	return r
 }
@@ -37,4 +46,15 @@ func getAllPatient(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("PQD get patient")
 	contacts := mh.Get(bson.M{})
 	json.NewEncoder(w).Encode(contacts)
+}
+func getPatientsByName(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("PQD get patient")
+	patients := mh.Get(bson.M{"name": bson.M{"$regex": `(?i).*` + "vm" + `.*`}})
+	resData := ResponseData{patients: patients, total: 3}
+	// render.JSON(w, r, resData)
+	fmt.Print("asd", resData.total)
+	a := json.NewEncoder(w)
+	// fmt.Print("a", a)
+	b := a.Encode(patients)
+	fmt.Print("b", b)
 }
