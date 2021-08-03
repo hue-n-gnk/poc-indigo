c6463df669b7075816247c94ecf238812108e8ab
Vo Minh Duc <duc-vm@genkisystem.com>
2021-08-04 00:14:51 +0700

    copy from backend

---
+++ packages/api/go.mod
@@ -0,0 +1,8 @@
+module poc/indigo
+
+go 1.13
+
+require (
+	github.com/go-chi/chi v4.1.2+incompatible
+	go.mongodb.org/mongo-driver v1.7.0
+)
---
+++ packages/api/repo/mongo.go
@@ -0,0 +1,55 @@
+package repo
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"poc/indigo/model"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const DefaultDatabase = "patient"
+
+type MongoHandler struct {
+	client   *mongo.Client
+	database string
+}
+
+//MongoHandler Constructor
+func NewHandler(address string) *MongoHandler {
+	ctx, err := context.WithTimeout(context.Background(), 10*time.Second)
+	fmt.Println("Error context: ", err)
+	cl, er := mongo.Connect(ctx, options.Client().ApplyURI(address))
+	fmt.Println("Result connect: ", er)
+	mh := &MongoHandler{
+		client:   cl,
+		database: DefaultDatabase,
+	}
+	return mh
+}
+
+func (mh *MongoHandler) Get(filter interface{}) []*model.Patient {
+	collection := mh.client.Database(mh.database).Collection("patient_info")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+	cur, err := collection.Find(ctx, filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(ctx)
+
+	var result []*model.Patient
+	for cur.Next(ctx) {
+		patient := &model.Patient{}
+		er := cur.Decode(patient)
+		if er != nil {
+			log.Fatal(er)
+		}
+		result = append(result, patient)
+	}
+	return result
+}
